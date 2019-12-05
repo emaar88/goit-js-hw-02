5139dd4dc6aba75320d81c82434a7e6d3bb00b6f
Сергей Сердюк <sergiy@emaar88@gmail.com>
2019-12-05 20:24:07 +0200

    good

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+'use strict';
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+function logItems(array) {
+  for (i = 1; i < length.logItems; i++) {
+    console.log(`${i} - ${logItems[i]}`);
+  }
+}
+
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
